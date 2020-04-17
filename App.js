534ba2dbe6a619e930e01fb5c83fa93a671b7ada
timager <timger98@gmail.com>
2020-04-17 18:37:27 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,60 @@
+'use strict';
+import React, {PureComponent} from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+
+const PendingView = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: 'lightgreen',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+    <Text>Waiting</Text>
+  </View>
+);
+
+class ExampleApp extends PureComponent {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>123</Text>
+        <RNCamera
+          style={styles.preview}
+          flashMode={RNCamera.Constants.FlashMode.torch}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+
+export default ExampleApp;
