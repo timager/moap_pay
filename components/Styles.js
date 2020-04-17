2b2b3de86f7184002cd612a8e06686a5c0511527
timager <timger98@gmail.com>
2020-04-17 21:17:04 +0300

    login page now so pretty

--- components/Styles.js
+++ components/Styles.js
@@ -2,14 +2,34 @@ import {StyleSheet, Dimensions} from 'react-native';
 
 export let styles = StyleSheet.create(
     {
-        bgBlue: {
-            backgroundColor: '#95a0e8',
+        bgColor: {
+            backgroundColor: '#d4d9ea',
         },
-        heightFull:{
+        heightFull: {
             height: Dimensions.get("window").height
         },
-        defaultFont:{
+        defaultFont: {
             fontSize: 30,
+        },
+        containerCenter: {
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center'
+        },
+        form: {
+            backgroundColor: '#84c6ff',
+            width: Dimensions.get("window").width * 0.8,
+            paddingLeft: 20,
+            paddingRight: 20,
+            paddingTop: 50,
+            paddingBottom: 50,
+            borderRadius: 20
+        },
+        formInput:{
+            borderRadius: 10,
+            marginBottom: 20,
+            backgroundColor: 'white'
         }
     },
 );
--- components/pages/LoginPage.js
+++ components/pages/LoginPage.js
@@ -1,12 +1,16 @@
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TextInput} from 'react-native';
 import React, {Component} from 'react';
 import {styles} from '../Styles'
 
 class LoginPage extends Component {
     render() {
         return (
-            <View style={[styles.heightFull, styles.bgBlue]}>
-                <Button style={styles.defaultFont} title={"123"}/>
+            <View style={[styles.heightFull, styles.bgColor,styles.containerCenter]}>
+                <View style={styles.form}>
+                    <TextInput style={styles.formInput} placeholder={"Логин"}/>
+                    <TextInput style={styles.formInput} placeholder={"Пароль"}/>
+                    <Button title={"Войти"} onPress={()=>{}}/>
+                </View>
             </View>
         );
     }
