c2c91590c68d4141c9255ebf7f5b80bd94392d8c
timager <timger98@gmail.com>
2020-04-21 23:52:51 +0300

    yandex api

--- components/pages/CameraPage.js
+++ components/pages/CameraPage.js
@@ -1,4 +1,4 @@
-import {View, Button, TouchableOpacity, Text, ImageBackground} from 'react-native';
+import {View, Button, TouchableOpacity, Text, ImageBackground, StyleSheet, ScrollView} from 'react-native';
 import React, {PureComponent} from 'react';
 import {styles} from '../Styles';
 import {RNCamera} from 'react-native-camera';
@@ -8,7 +8,9 @@ class CameraPage extends PureComponent {
     constructor() {
         super();
         this.state = {
-            url: ''
+            url: '',
+            base64: '',
+            response: "wait"
         }
     }
 
@@ -31,17 +33,48 @@ class CameraPage extends PureComponent {
                         <Button onPress={this.takePicture.bind(this)} title={'Отправить'}/>
                     </View>
                 </ImageBackground>
+                <ScrollView style={styles.h200}>
+                    <Text style={styles.bgColor}>{this.state.response}</Text>
+                </ScrollView>
             </View>
         );
     }
 
     takePicture = async () => {
         if (this.camera) {
-            const options = {base64: true};
-            let url = await this.camera.takePictureAsync(options);
-            this.setState({url: url});
+            const options = {base64: true, quality: 0.5};
+            let data = await this.camera.takePictureAsync(options);
+            this.setState({url: data, base64: data.base64}, this.sendToApi);
         }
     };
+
+    sendToApi() {
+        let body = JSON.stringify({
+            // "folderId": "b1gh07ni0nkc3fvjr18g",
+            "analyze_specs": [{
+                "content": this.state.base64,
+                "features": [{
+                    "type": "TEXT_DETECTION",
+                    "text_detection_config": {
+                        "language_codes": ["ru"]
+                    }
+                }]
+            }]
+        });
+        fetch(
+            'https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze',
+            {
+                method: "POST",
+                headers: {
+                    'Content-Type': "application/json",
+                    'Authorization': "Api-Key AQVN30U4ze8tyv3Khw3fZ-IKU3LghOHwd5fwKic_"
+                },
+                body: body
+            }
+        )
+            .then((response) => response.text())
+            .then((response) => this.setState({response: response}))//.results[0].results
+    }
 }
 
 export default CameraPage;
