cf045eeba38b233b01902eb637bbd34780a396c5
nik <nik_mak@bk.ru>
2020-05-12 18:03:26 +0300

    rename app on android

--- android/app/src/main/java/com/automer/MainActivity.java
+++ android/app/src/main/java/com/automer/MainActivity.java
@@ -1,4 +1,4 @@
-package com.moek_pay;
+package com.automer;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "moek_pay";
+    return "automer";
   }
 }
--- android/app/src/main/java/com/automer/MainApplication.java
+++ android/app/src/main/java/com/automer/MainApplication.java
@@ -1,4 +1,4 @@
-package com.moek_pay;
+package com.automer;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.moek_pay.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.automer.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
